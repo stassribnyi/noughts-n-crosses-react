dd3888ad5c00836d48d21d902d7fda5640d57a34
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-02-26 23:27:25 +0200

    Updates

---
+++ precache-manifest.57b73e172fcd3f18760acd7b4869ef87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a4a91f6d9c3c5417b5e",
+    "url": "/noughts-n-crosses-react/static/js/runtime~main.9a4a91f6.js"
+  },
+  {
+    "revision": "74849224f8a1749cf746",
+    "url": "/noughts-n-crosses-react/static/js/main.74849224.chunk.js"
+  },
+  {
+    "revision": "ea089c583e222e215502",
+    "url": "/noughts-n-crosses-react/static/js/2.ea089c58.chunk.js"
+  },
+  {
+    "revision": "74849224f8a1749cf746",
+    "url": "/noughts-n-crosses-react/static/css/main.cfc60596.chunk.css"
+  },
+  {
+    "revision": "7e35b60c914f58c4deed6053804ab328",
+    "url": "/noughts-n-crosses-react/index.html"
+  }
+];
